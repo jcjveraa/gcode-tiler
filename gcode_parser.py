b489fb3df73b9aa47ed7ee5b22ac022f5944359c
JelleV <3942301+jcjveraa@users.noreply.github.com>
2023-02-13 17:08:44 +0100

    next steps...

--- gcode_parser.py
+++ gcode_parser.py
@@ -1,14 +1,15 @@
+import math
 import re
-
 
 last_x = 0
 last_y = 0
 x_coord_regex = r"X([\-0-9\.]+)"
 y_coord_regex = r"Y([\-0-9\.]+)"
 
+
 def get_coord(matcher, line):
     match = re.search(matcher, line)
-    return match.groups(0) if match else None
+    return match.groups(0)[0] if match else None
 
 
 def parse_gcode_line(line: str):
@@ -16,7 +17,14 @@ def parse_gcode_line(line: str):
     using_spaces = re.search(r"\s", line) is not None
     sep = ' ' if using_spaces else ''
     x, y = get_coord(x_coord_regex, line), get_coord(y_coord_regex, line)
-    print(x, y, sep=sep)
+    return x, y, line
+
+
+def update_gcode_line_xy(x: float, y: float, line: str, precision=4) -> str:
+    line = re.sub(x_coord_regex, 'X' + str(round(x, precision)), line)
+    line = re.sub(y_coord_regex, 'Y' + str(round(y, precision)), line)
+
+    return line
 
 
 parse_gcode_line("G1 X22.7508 Y-21.8884")
@@ -24,3 +32,6 @@ parse_gcode_line("G1X23.7508Y-21.8884")
 parse_gcode_line("G1X22.7508Y-21.8884")
 parse_gcode_line("G1Y-21.8884")
 parse_gcode_line("G1X-21.8884")
+
+print(update_gcode_line_xy(math.pi, -math.e, "G1X22.7508Y-21.8884"))
+print(update_gcode_line_xy(math.pi, -math.e, "G1 X22.7508 Y-21.8884"))
--- gcode_tiler.py
+++ gcode_tiler.py
@@ -4,7 +4,7 @@ rounding_precision = 4
 
 
 def rotate_point_around_origin(
-    point: tuple[float, float], angle: float, degrees=True, round_result=False
+        point: tuple[float, float], angle: float, degrees=True, round_result=False
 ) -> tuple[float, float]:
     if degrees:
         angle = angle / 180 * math.pi
@@ -18,21 +18,21 @@ def rotate_point_around_origin(
         new_x = round(new_x, rounding_precision)
         new_y = round(new_y, rounding_precision)
 
-    return (new_x, new_y)
+    return new_x, new_y
 
 
 def rotate_point_around_point(
-    point: tuple[float, float],
-    rotation_point: tuple[float, float],
-    angle: float,
-    degrees=True,
-    round_result=False,
+        point: tuple[float, float],
+        rotation_point: tuple[float, float],
+        angle: float,
+        degrees=True,
+        round_result=False,
 ) -> tuple[float, float]:
     center_x, center_y = rotation_point[0], rotation_point[1]
     # translate to origin
     x, y = point[0] - center_x, point[1] - center_y
     # rotate
-    x, y = rotate_point_around_origin(tuple([x, y]), angle=angle, degrees=degrees)
+    x, y = rotate_point_around_origin((x, y), angle=angle, degrees=degrees)
     # translate back
     x, y = x + center_x, y + center_y
 
@@ -40,8 +40,8 @@ def rotate_point_around_point(
         x = round(x, rounding_precision)
         y = round(y, rounding_precision)
 
-    return (x, y)
+    return x, y
 
 
-print(rotate_point_around_origin(tuple([0, 2]), 90))
-print(rotate_point_around_point(tuple([0, 2]), tuple([0, 1]), 90))
+print(rotate_point_around_origin((0, 2), 90))
+print(rotate_point_around_point((0, 2), (0, 1), 90))
