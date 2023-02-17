6e6580d40f89b70ff4ceb177530165f63c209aa5
JelleV <3942301+jcjveraa@users.noreply.github.com>
2023-02-17 13:54:01 +0100

    working example :)

--- runner.py
+++ runner.py
@@ -2,16 +2,19 @@ from gcode_parser import parse_gcode_line, update_gcode_line_xy
 from gcode_tiler import rotate_point_around_origin, translate_point
 
 input = open("./example/kitty_edited_jscut.gcode")
-output = open("./example/kitty_edited_jscut_edited.gcode", "w")
+output = open("./example/kitty_edited_jscut_rotate_translate.gcode", "w")
 
-for line in input.readlines():
-    x, y, line = parse_gcode_line(line)
-    if x is not None:
-        x, y = rotate_point_around_origin((x, y), 180)
-        x, y = translate_point((x, y), (50, 0))
-        line = update_gcode_line_xy(x,y,line)
-    output.write(line + '\n')
-    
+
+for copy in range(4):
+    for line in input.readlines():
+        x, y, line = parse_gcode_line(line)
+        if x is not None:
+            angle_degrees = 180 if copy%2 == 1 else 0
+            x, y = rotate_point_around_origin((x, y), angle_degrees)
+            x, y = translate_point((x, y), (copy*25, 0))
+            line = update_gcode_line_xy(x,y,line)
+        output.write(line + '\n')
+    input.seek(0)
 
 input.close()
 output.close()
