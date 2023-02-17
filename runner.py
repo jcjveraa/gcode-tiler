c92c4309ddbe41a8fee3828034a87d67a8a44987
JelleV <3942301+jcjveraa@users.noreply.github.com>
2023-02-17 13:56:09 +0100

    fix formatting

--- runner.py
+++ runner.py
@@ -9,10 +9,10 @@ for copy in range(4):
     for line in input.readlines():
         x, y, line = parse_gcode_line(line)
         if x is not None:
-            angle_degrees = 180 if copy%2 == 1 else 0
+            angle_degrees = 180 if copy % 2 == 1 else 0
             x, y = rotate_point_around_origin((x, y), angle_degrees)
             x, y = translate_point((x, y), (copy*25, 0))
-            line = update_gcode_line_xy(x,y,line)
+            line = update_gcode_line_xy(x, y, line)
         output.write(line + '\n')
     input.seek(0)
 
